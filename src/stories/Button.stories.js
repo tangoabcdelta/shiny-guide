d52cfa0dab882a050e324778612881d598f7171d
foobar@femail.com <femail@foobar.com>
2020-11-20 17:25:03 +0530

    -

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
-
-export default {
-  title: 'Example/Button',
+import { Button } from "./Button";
+const ExampleButton = {
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: "Button",
 };
 
-export const Secondary = Template.bind({});
+const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
-export const Large = Template.bind({});
+const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
-export const Small = Template.bind({});
+const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
+
+export { Primary, Secondary, Large, Small, ExampleButton };
+export default Button;
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { Header } from './Header';
+import { Header } from "./Header";
 
-export default {
-  title: 'Example/Header',
+const ExampleHeader = {
+  title: "Example/Header",
   component: Header,
 };
 
 const Template = (args) => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
+const LoggedIn = Template.bind({});
 LoggedIn.args = {
   user: {},
 };
 
-export const LoggedOut = Template.bind({});
+const LoggedOut = Template.bind({});
 LoggedOut.args = {};
+
+export { LoggedIn, LoggedOut };
+export default ExampleHeader;
